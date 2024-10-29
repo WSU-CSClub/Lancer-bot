891c29c0d4e9345911334ecd8bbb8ff7aad75e13
Isacs Macbook <Iwandiba@yahoo.com>
2024-10-29 15:23:17 -0400

    fixing pushes and checking moves

--- src/engine/movegen.hpp
+++ src/engine/movegen.hpp
@@ -21,16 +21,16 @@ private:
     std::vector<Move> moves;
 
     uint64_t getRankMask(int square) {
-/*
-Rank 1: 0xFF               // 0-7
-Rank 2: 0xFF00            // 8-15    (0xFF << 8)
-Rank 3: 0xFF0000          // 16-23   (0xFF << 16)
-Rank 4: 0xFF000000        // 24-31   (0xFF << 24)
-Rank 5: 0xFF00000000      // 32-39   (0xFF << 32)
-Rank 6: 0xFF0000000000    // 40-47   (0xFF << 40)
-Rank 7: 0xFF000000000000  // 48-55   (0xFF << 48)
-Rank 8: 0xFF00000000000000// 56-63   (0xFF << 56)
-*/ 
+    /*
+    Rank 1: 0xFF               // 0-7
+    Rank 2: 0xFF00            // 8-15    (0xFF << 8)
+    Rank 3: 0xFF0000          // 16-23   (0xFF << 16)
+    Rank 4: 0xFF000000        // 24-31   (0xFF << 24)
+    Rank 5: 0xFF00000000      // 32-39   (0xFF << 32)
+    Rank 6: 0xFF0000000000    // 40-47   (0xFF << 40)
+    Rank 7: 0xFF000000000000  // 48-55   (0xFF << 48)
+    Rank 8: 0xFF00000000000000// 56-63   (0xFF << 56)
+    */ 
         return 0xFFULL << (square & 56);  // square & 56 is same as (square / 8) * 8
     }
     
@@ -45,8 +45,6 @@ Rank 8: 0xFF00000000000000// 56-63   (0xFF << 56)
                board[BP] | board[BN] | board[BB] | board[BR] | board[BQ] | board[BK]; 
     }
 
-
- 
     uint64_t getWhitePieces(){
         return board[WP] | board[WN] | board[WB] | board[WR] | board[WQ] | board[WK];
     }
@@ -61,20 +59,26 @@ Rank 8: 0xFF00000000000000// 56-63   (0xFF << 56)
         uint64_t enemies = isWhite ? getBlackPieces() : getWhitePieces();
         uint64_t empty = ~getAllPieces();
 
-        int direction = isWhite ? 8 : -8;
 
-        uint64_t SinglePush = (pawns << direction) & empty;
+        // Bits dont work with negatives 
+        int direction = isWhite ? 8 : -8;
         
-        uint64_t doublePush = ((SinglePush & (isWhite ? 0xFF000000 : 0xFF0000)) << direction) & empty;
-        
+        uint64_t SinglePush = isWhite ? 
+            (pawns << 8) & empty :    // White moves up
+            (pawns >> 8) & empty;         
+
+        // -.- make sure to EXPLICITY call it 64 bits 
+        uint64_t doublePush = isWhite ? 
+            ((SinglePush & 0xFF0000ULL) << 8) & empty :            // White pawns on rank 3
+            ((SinglePush & 0xFF0000000000ULL) >> 8) & empty;   // Isolate rank 6
 
         //~0x0101010101010101ULL Makes sure pawn cant capture on the A File  
         //~0x8080808080808080ULL Make sure Pawn Cant Capture on the H File 
-        uint64_t left_capture = isWhite ? 
+        uint64_t LeftCapture = isWhite ? 
             ((pawns & ~0x0101010101010101ULL) << 7) & enemies :
             ((pawns & ~0x0101010101010101ULL) >> 9) & enemies;
 
-        uint64_t rightCaptures = isWhite ?
+        uint64_t RightCaptures = isWhite ?
             ((pawns & ~0x8080808080808080ULL) << 9) & enemies :
             ((pawns & ~0x8080808080808080ULL) >> 7) & enemies;
 
@@ -84,22 +88,42 @@ Rank 8: 0xFF00000000000000// 56-63   (0xFF << 56)
         // Essentially jump to next ON BIT, instead of iterating through all of them discuss at Meeting (Isac)
         // __builtin_ctzll Essentailly Finds the Right most Bit
         // Save the Push 
-        // SingePush - 1 removes the move and then looks for the next move again :) 
+        // SingePush - 1 removes the move and then looks for the next move again 
         while(SinglePush) {
             int to = __builtin_ctzll(SinglePush);
             int from = to - direction;
             moves.push_back({(uint8_t)from, (uint8_t)to, 0});
             SinglePush &= SinglePush - 1;  // Clear least significant bit
         }
+        
+        while(doublePush) {
+            int to = __builtin_ctzll(doublePush);
+            int from = to - (2 * direction);  // Subtract 2 ranks worth of movement
+            moves.push_back({(uint8_t)from, (uint8_t)to, 0});
+            doublePush &= doublePush - 1; 
+        }
 
+        while(LeftCapture){
+            int to = __builtin_ctzll(LeftCapture);
+            int from = isWhite ? to - 7 : to + 9;  
+            moves.push_back({(uint8_t)from, (uint8_t)to, 0});
+            LeftCapture &= LeftCapture - 1; 
+        }
 
-        //Need to Write Double,Left & Right Capture :) 
-
+        while(RightCaptures){
+            int to = __builtin_ctzll(RightCaptures);
+            int from = isWhite ? to - 9 : to + 7;  // Correct diagonal math
+            moves.push_back({(uint8_t)from, (uint8_t)to, 0});
+            RightCaptures &= RightCaptures - 1; 
+        }
     }
     public:
     MoveGen(ChessBoard& boards) : board(boards){}
 
-    void GenerateMoves(){
-
+    std::vector<Move> GenerateMoves(bool isWhite) {
+        moves.clear();
+        GeneratePawnMoves(isWhite);
+        // generateKnightMoves(isWhite);
+        return moves;
     }
 };
--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,18 @@
 #include "engine/board.hpp"
+#include "engine/movegen.hpp"
 #include <cstdio>
+
+
+void printMove(const Move& move) {
+    // Convert square numbers to chess notation
+    char fromFile = 'a' + (move.from % 8);
+    int fromRank = 1 + (move.from / 8);
+    char toFile = 'a' + (move.to % 8);
+    int toRank = 1 + (move.to / 8);
+    
+    printf("%c%d%c%d ", fromFile, fromRank, toFile, toRank);
+}
+
 
 int main() {
   // Example bitboard
@@ -9,5 +22,27 @@ int main() {
   // Print the board
   printBoard(board);
 
+
+MoveGen moveGen(board);
+
+
+
+    // Generate moves for white
+    std::vector<Move> whiteMoves = moveGen.GenerateMoves(true);
+    
+    // Print all white moves
+    printf("\nWhite moves:\n");
+    for(const Move& move : whiteMoves) {
+        printMove(move);
+    }
+    
+    // Generate moves for black
+    std::vector<Move> blackMoves = moveGen.GenerateMoves(false);
+    
+    // Print all black moves
+    printf("\n\nBlack moves:\n");
+    for(const Move& move : blackMoves) {
+        printMove(move);
+    }
   return 0;
 }
